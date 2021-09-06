8e68d602dd69ae78d978c23bb71982b9f5dcd2bc
AlexCXC <1223408988@qq.com>
2021-09-06 15:43:10 +0800

    update thread Emails by seatable.add_link one by one

--- email_sync/email_syncer.py
+++ email_sync/email_syncer.py
@@ -225,9 +225,11 @@ def update_email_thread_ids(seatable, email_table_name, send_date, email_list):
     message2thread = {email['Message ID']: email['Thread ID'] for email in email_rows}
     email_list = [email for email in email_list if email['Message ID'] not in message2thread]
 
-    # checkout reply messages that not in last 30 days and checkout those email rows from seatable by sql
-    no_thread_reply_message_ids = [email['Reply to Message ID'] for email in email_list \
-                                   if email['Reply to Message ID'] not in message2thread]
+    # no_thread_reply_message_ids is the list of new emails' reply-ids who are not in last 30 days
+    no_thread_reply_message_ids = []
+    for email in email_list:
+        if email['Reply to Message ID'] and email['Reply to Message ID'] not in message2thread:
+            no_thread_reply_message_ids.append(email['Reply to Message ID'])
     if no_thread_reply_message_ids:
         step = 100
         for i in range(0, len(no_thread_reply_message_ids), step):
@@ -236,34 +238,58 @@ def update_email_thread_ids(seatable, email_table_name, send_date, email_list):
             earlier_email_rows = query_table_rows(seatable, email_table_name,
                                                   fields='`Message ID`, `Thread ID`',
                                                   conditions=conditions,
+                                                  all=False,
                                                   limit=step)
             for email in earlier_email_rows:
                 message2thread[email['Message ID']] = email['Thread ID']
 
+    new_thread_rows = []
+    to_be_updated_thread_dict = {}
     # update email thread id
     for email in email_list:
         reply_to_id = email['Reply to Message ID']
         message_id = email['Message ID']
         if reply_to_id in message2thread:  # checkout thread id from old message2thread
-            message2thread[message_id] = message2thread[reply_to_id]
+            thread_id = message2thread[reply_to_id]
+            message2thread[message_id] = thread_id
+            if thread_id in to_be_updated_thread_dict:
+                # update Last Updated
+                if str_2_datetime(email['Date']) > str_2_datetime(to_be_updated_thread_dict[thread_id]['Last Updated']):
+                    to_be_updated_thread_dict[thread_id]['Last Updated'] = email['Date']
+                # append email message id
+                to_be_updated_thread_dict[thread_id]['message_ids'].append(message_id)
+            else:
+                to_be_updated_thread_dict[thread_id] = {
+                    'Last Updated': email['Date'],
+                    'message_ids': [message_id]
+                }
         else:  # generate new thread id
             thread_id = uuid4().hex
             message2thread[message_id] = thread_id
-    for email in email_list:
-        email['Thread ID'] = message2thread[email['Message ID']]
+            new_thread_rows.append({
+                'Subject': email['Subject'],
+                'Last Updated': email['Date'],
+                'Thread ID': thread_id
+            })
+            to_be_updated_thread_dict[thread_id] = {
+                'Last Updated': email['Date'],
+                'message_ids': [message_id]
+            }
+        email['Thread ID'] = message2thread[message_id]
 
-    return email_list
+    return email_list, new_thread_rows, to_be_updated_thread_dict
 
 
 def fill_email_list_with_row_id(seatable, email_table_name, email_list):
     step = 100
     message_id_row_id_dict = {}  # {message_id: row._id}
     for i in range(0, len(email_list), step):
-        message_ids_str = ', '.join([f"'{email['Message ID']}'" for email in email_list])
+        message_ids_str = ', '.join([f"'{email['Message ID']}'" for email in email_list[i: i+step]])
         conditions = f'`Message ID` in ({message_ids_str})'
         email_rows = query_table_rows(seatable, email_table_name,
                                       fields='`_id`, `Message ID`',
                                       conditions=conditions,
+                                      all=False,
                                       limit=step)
         message_id_row_id_dict.update({row['Message ID']: {
             '_id': row['_id'],
@@ -273,85 +299,42 @@ def fill_email_list_with_row_id(seatable, email_table_name, email_list):
     return email_list
 
 
-def update_and_append_threads(seatable: SeaTableAPI, email_table_name, link_table_name, email_list, send_date):
+def update_threads(seatable: SeaTableAPI, email_table_name, link_table_name, email_list, to_be_updated_thread_dict):
     """
-    email_list: not with row id and need to be filled with row id
     update thread table
+    email_list: list of email
+    to_be_updated_thread_dict: {thread_id: {'Last Updated': 'YYYY-MM-DD', 'message_ids': [message_id1, message_id2...]}}
     """
-    # checkout thread rows that are in email[Thread ID] of email_list
-    time.sleep(5)  # sleep, waiting for dtable-db
-    email_list = fill_email_list_with_row_id(seatable, email_table_name, email_list)
-    thread_ids = list(set([email['Thread ID'] for email in email_list]))
+    to_be_updated_thread_ids = list(to_be_updated_thread_dict.keys())
+    thread_id_row_id_dict = {}
     step = 100
-    thread_emails_dict = {}  # {thread_id: {_id, Thread ID, Emails, Last Updated...}}
-    for i in range(0, len(thread_ids), step):
-        thread_ids_str = ', '.join([f"'{thread_id}'" for thread_id in thread_ids[i: i+step]])
+    for i in range(0, len(to_be_updated_thread_ids), step):
+        thread_ids_str = ', '.join([f"'{thread_id}'" for thread_id in to_be_updated_thread_ids[i: i+step]])
         conditions = f"`Thread ID` in ({thread_ids_str})"
         thread_rows = query_table_rows(seatable, link_table_name,
-                                       fields='`Thread ID`, `Emails`, `Last Updated`, `_id`',
+                                       fields='`Thread ID`, `_id`',
                                        conditions=conditions,
+                                       all=False,
                                        limit=step)
-        thread_emails_dict.update({row['Thread ID']: {
-            '_id': row['_id'],
-            'Thread ID': row['Thread ID'],
-            'Emails': row.get('Emails', []),  # There is a bug, sql query can't return Link cell value
-            'Last Updated': row['Last Updated']
-        } for row in thread_rows})
+        thread_id_row_id_dict.update({row['Thread ID']: row['_id'] for row in thread_rows})
 
-    # gen `Last Updated` update-data and new thread rows
-    need_update_last_updated_rows = []
-    new_thread_rows, new_thread_dict = [], {}
-    to_be_added_links = []
-    for email in email_list:
-        # if email's thread in old thread-rows, need to be updated `Last Updated` if Date > Last Updated
-        if email['Thread ID'] in thread_emails_dict:
-            if str_2_datetime(thread_emails_dict[email['Thread ID']]['Last Updated']) < str_2_datetime(email['Date']):
-                need_update_last_updated_rows.append({
-                    'row_id': thread_emails_dict[email['Thread ID']]['_id'],
-                    'row': {'Last Updated': email['Date']}
-                })
-            if email['_id'] not in thread_emails_dict[email['Thread ID']].get('Emails', []):
-                thread_emails_dict[email['Thread ID']]['Emails'] = thread_emails_dict[email['Thread ID']].get('Emails', []) + [email['_id']]
-                to_be_added_links.append((thread_emails_dict[email['Thread ID']]['_id'], email['_id']))
-        else:  # append new thread row
-            if email['Thread ID'] not in new_thread_dict:  # if thread not recorded, append new thread row
-                new_thread_rows.append({
-                    'Subject': email['Subject'],
-                    'Last Updated': email['Date'],
-                    'Thread ID': email['Thread ID']
-                })
-                new_thread_dict[email['Thread ID']] = len(new_thread_rows) - 1
-            else:  # if thread recorded, need to check email date is whether earilier than recored thread
-                if str_2_datetime(email['Date']) < str_2_datetime(new_thread_rows[new_thread_dict[email['Thread ID']]]['Last Updated']):
-                    new_thread_rows[new_thread_dict[email['Thread ID']]]['Last Updated'] = email['Date']
-                    new_thread_rows[new_thread_dict[email['Thread ID']]]['Subject'] = email['Subject']
+    # batch update Last Updated
+    to_be_updated_last_updated_rows = [{
+        'row_id': thread_id_row_id_dict[key],
+        'row': {'Last Updated': value['Last Updated']}
+    } for key, value in to_be_updated_thread_dict.items()]
+    seatable.batch_update_rows(link_table_name, to_be_updated_last_updated_rows)
 
-        # batch append new thread rows
-    if new_thread_rows:
-        logger.info(f'insert {len(new_thread_rows)} thread rows')
-        seatable.batch_append_rows(link_table_name, new_thread_rows)
-    # batch update links
-    new_other_row_ids_map = {}
-    new_thread_rows = query_table_rows(seatable, link_table_name, fields='`Thread ID`, `_id`', conditions=f"`Last Updated`>='{send_date}'")
-    new_thread_id_row_id_dict = {row['Thread ID']: row['_id'] for row in new_thread_rows}
-    for email in email_list:
-        if email['Thread ID'] in new_thread_id_row_id_dict:
-            if new_thread_id_row_id_dict[email['Thread ID']] in new_other_row_ids_map:
-                new_other_row_ids_map[new_thread_id_row_id_dict[email['Thread ID']]].append(email['_id'])
-            else:
-                new_other_row_ids_map[new_thread_id_row_id_dict[email['Thread ID']]] = [email['_id']]
-    metadata = seatable.get_metadata()
-    table_info = {table['name']: table['_id'] for table in metadata['tables']}
-    table_id = table_info[link_table_name]
-    other_table_id = table_info[email_table_name]
+    # fill email in email_list with row id
+    email_list = fill_email_list_with_row_id(seatable, email_table_name, email_list)
+    email_dict = {email['Message ID']: email for email in email_list}
+    # add link
     link_id = seatable.get_column_link_id(link_table_name, 'Emails', view_name=None)
-    seatable.batch_update_links(link_id, table_id, other_table_id, list(new_other_row_ids_map.keys()), new_other_row_ids_map)
-    # batch update `Last Updated`
-    if need_update_last_updated_rows:
-        seatable.batch_update_rows(link_table_name, need_update_last_updated_rows)
-    # add thread link
-    for thread_row_id, email_row_id in to_be_added_links:
-        seatable.add_link(link_id, link_table_name, email_table_name, thread_row_id, email_row_id)
+    for key, value in to_be_updated_thread_dict.items():
+        thread_id = key
+        row_id = thread_id_row_id_dict[thread_id]
+        for message_id in value['message_ids']:
+            seatable.add_link(link_id, link_table_name, email_table_name, row_id, email_dict[message_id]['_id'])
 
 
 def sync(send_date, mode='ON'):
@@ -367,16 +350,19 @@ def sync(send_date, mode='ON'):
         seatable.auth()
 
         # update thread id of emails
-        email_list = update_email_thread_ids(seatable, settings.EMAIL_TABLE_NAME, send_date, email_list)
+        email_list, new_thread_rows, to_be_updated_thread_dict = update_email_thread_ids(seatable, settings.EMAIL_TABLE_NAME, send_date, email_list)
         logger.info(f'need to be inserted {len(email_list)} emails')
+        logger.info(f'need to be inserted {len(new_thread_rows)} thread rows')
         if not email_list:
             return
 
         # insert new emails
         seatable.batch_append_rows(settings.EMAIL_TABLE_NAME, email_list)
+        # insert new thread rows
+        seatable.batch_append_rows(settings.LINK_TABLE_NAME, new_thread_rows)
 
-        # update threads from new emails
-        update_and_append_threads(seatable, settings.EMAIL_TABLE_NAME, settings.LINK_TABLE_NAME, email_list, send_date)
+        # update threads Last Updated and Emails
+        update_threads(seatable, settings.EMAIL_TABLE_NAME, settings.LINK_TABLE_NAME, email_list, to_be_updated_thread_dict)
     except Exception as e:
         logger.exception(e)
         logger.error('sync and update link error: %s', e)
