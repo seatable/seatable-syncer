1399b7fe671bdb674a81922c6239e08dcfc7a03b
skywalker <dzmbbs@qq.com>
2021-09-13 15:21:43 +0800

    log-sync

---
+++ log-sync/log_syncer.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import re
+import time
+import json
+import redis
+from datetime import datetime
+from seatable_api import Base
+
+from settings import server_url, api_token, table_name, filebeat_key, \
+    redis_host, redis_port, redis_db, redis_password
+
+
+DATETIME_MATCH_1 = r'(\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2})'
+DATETIME_MATCH_2 = r'(\d{4}-\d{1,2}-\d{1,2}T\d{1,2}:\d{1,2}:\d{1,2})'
+
+
+class LogSyncer(object):
+
+    def __init__(self):
+        self.base = Base(api_token, server_url)
+        self.r = redis.Redis(
+            host=redis_host, port=redis_port, db=redis_db, password=redis_password)
+
+    def now(self):
+        return str(datetime.now())
+
+    def count(self):
+        return self.r.llen(filebeat_key)
+
+    def send(self, log):
+        log = json.loads(log)
+        msg = log['message']
+        if re.search(DATETIME_MATCH_1, msg):
+            log_time = re.search(
+                DATETIME_MATCH_1, msg).group(1)
+        elif re.search(DATETIME_MATCH_2, msg):
+            log_time = re.search(
+                DATETIME_MATCH_2, msg).group(1).replace('T', ' ')
+        else:
+            log_time = self.now()
+        msg = '```\n' + msg + '\n```'
+        tag = '-'.join(log['tags'])
+        print(log_time, tag, log)
+
+        row_data = {'Tag': tag, 'Time': log_time, 'Log': msg}
+        self.base.append_row(table_name, row_data)
+
+    def start(self):
+        print(self.now(), 'Logs count:', self.count())
+        self.base.auth()
+
+        while True:
+            try:
+                log = self.r.lpop(filebeat_key)
+                if not log:
+                    time.sleep(30)
+                    continue
+
+                self.send(log)
+                time.sleep(0.5)
+            except Exception as e:
+                print(self.now(), e)
+                time.sleep(10)
+                self.base.auth()
+
+
+if __name__ == '__main__':
+    log_syncer = LogSyncer()
+    log_syncer.start()
