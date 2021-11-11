b99b802bb54b7d6581f52a33fb2aea92e33f0ba7
‘JoinTyang’ <yangtong1009@163.com>
2021-11-11 17:41:30 +0800

    fix sql query

--- mysql_sync/mysql_syncer.py
+++ mysql_sync/mysql_syncer.py
@@ -1,4 +1,6 @@
 import json
+import time
+
 import pymysql
 import logging
 import settings
@@ -7,6 +9,7 @@ from seatable_api import Base
 import os
 from bs4 import BeautifulSoup
 import markdown
+import re
 
 
 logging.basicConfig(
@@ -41,23 +44,39 @@ class DataUtil(object):
         field_info = self.cursor.fetchall()
         self.field_info = {field['Field']: field['Type'].split('(')[0].upper() for field in field_info}
 
-    def get_unique_rows(self):
+    def get_base_unique_rows(self):
         return fixed_sql_query(self.base, f"select {self.unique_field} from `{self.table_name}`")
 
+    def check_base(self):
+        base_count_info = fixed_sql_query(self.base, f"select count(*) as base_count from `{self.table_name}`")
+        return True if base_count_info[0].get('base_count') else False
+
+    def get_mysql_row_count(self):
+        count_sql = "SELECT COUNT(*) AS total_count FROM " + self.table_name
+        self.cursor.execute(count_sql)
+        return self.cursor.fetchone().get('total_count')
+
     def get_mysql_data(self):
-        unique_rows = ','.join([str(row[self.unique_field]) for row in self.get_unique_rows()])
-
         sql = "SELECT " + ','.join(["`%s`" % key for key in self.field_info.keys()]) + " FROM " + self.table_name
-        if self.mode == "ON":
-            sql += " WHERE " + "date(`" + settings.DATE_FIELD + "`) = date_sub(curdate(),interval 1 day)"
-            if unique_rows:
-                sql += "AND `%s` not in (%s)" % (self.unique_field, unique_rows)
+        if self.mode == 'ON':
+            sql += " WHERE " + "DATE(`" + settings.DATE_FIELD + "`) = date_sub(curdate(),interval 1 day)"
+        mysql_rows = []
+        step = 1000
+        i = 0
+        while True:
+            query_sql = sql + f" LIMIT {i*step},{step}"
+            self.cursor.execute(query_sql)
+            rows = self.cursor.fetchall()
+            time.sleep(0.5)
+            mysql_rows += rows
+            if len(rows) < step:
+                break
+            i += 1
+        if self.mode == 'ON':
+            unique_rows = [row[self.unique_field] for row in self.get_base_unique_rows()]
+            mysql_rows = [row for row in mysql_rows if row[self.unique_field] not in unique_rows]
 
-        if unique_rows and self.mode == 'ALL':
-            sql += " WHERE `%s` not in (%s)" % (self.unique_field, unique_rows)
-        self.cursor.execute(sql)
-
-        return self.parse_mysql_rows(self.cursor.fetchall())
+        return self.parse_mysql_rows(mysql_rows)
 
     def parse_mysql_rows(self, mysql_rows):
         for row in mysql_rows:
@@ -94,25 +113,35 @@ def get_preview_info(preview_content):
     bf = BeautifulSoup(preview_content, features="lxml")
     links = []
     images = []
+    checked_count = 0
+    unchecked_count = 0
     for link in bf.find_all(name='a'):
         links.append(link.attrs.get('href'))
     for img in bf.find_all(name='img'):
         images.append(img.attrs.get('src'))
+    for check_text in bf.find_all(name='li'):
+        if check_text.text.startswith('[x]'):
+            checked_count += 1
+        elif check_text.text.startswith('[ ]'):
+            unchecked_count += 1
 
-    return links, images
+    return links, images, checked_count, unchecked_count
 
 
 def parse_long_text(cell_value):
     if not cell_value:
         return {}
     preview_content = markdown.markdown(cell_value)
-    links, images = get_preview_info(preview_content)
-    checked_count = cell_value.count('[x]')
-    unchecked_count = cell_value.count('[ ]')
+    links, images, checked_count, unchecked_count = get_preview_info(preview_content)
     total = checked_count + unchecked_count
 
+    HREF_REG = r'\[.+\]\(\S+\)|<img src=\S+.+\/>|!\[\]\(\S+\)|<\S+>'
+    href_reg = re.compile(HREF_REG)
+    preview = href_reg.sub(' ', cell_value)
+    preview = preview[:20].replace('\n', ' ')
     return {
             'text': cell_value,
+            'preview': preview,
             'checklist': {'completed': checked_count, 'total': total},
             'images': images,
             'links': links,
@@ -129,17 +158,20 @@ def fixed_sql_query(seatable, sql):
 def get_mysql_rows(base, mode, table_name, unique_field, username, password, db_name, host, charset):
     mysql_data = DataUtil(base, mode, table_name, unique_field, username, password, db_name, host, charset)
     try:
+        if mode == 'ALL':
+            if mysql_data.check_base():
+                logger.error(f'base table {table_name} should be empty if mode is ALL')
+                return []
+
         mysql_data.get_mysql_field_info()
         if mysql_to_dtable_dict[mysql_data.field_info[mysql_data.unique_field]] not in ('number', 'text'):
             logger.error('unique field type error')
-        mysql_rows = mysql_data.get_mysql_data()
+            return []
+        return mysql_data.get_mysql_data()
     except Exception as e:
         logger.exception(e)
-    else:
-        return mysql_rows
     finally:
         mysql_data.close()
-    return []
 
 
 def sync(api_token,
